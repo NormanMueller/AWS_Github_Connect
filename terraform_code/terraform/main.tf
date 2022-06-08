89836d0fec22295f0d6d68b30915b7f48bc7f8b1
NormanMueller <93076064+NormanMueller@users.noreply.github.com>
2022-06-08 21:16:25 +0200

    doacker and terraform integration

---
+++ terraform_code/terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+resource "aws_budgets_budget" "monthly-budget" {
+  name              = "monthly-budget"
+  budget_type       = "COST"
+  limit_amount      = "30"
+  limit_unit        = "USD"
+  time_period_start = "2021-09-01_00:00"
+  time_unit         = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 100
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["user@example.com"]
+  }
+}
