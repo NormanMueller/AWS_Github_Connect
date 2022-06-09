88d8ba45390b951bce1fcbc689c0d24609f428ff
NormanMueller <93076064+NormanMueller@users.noreply.github.com>
2022-06-09 17:50:43 +0200

    fmt terraform

--- terraform_code/terraform/iam.tf
+++ terraform_code/terraform/iam.tf
@@ -8,3 +8,7 @@ data "aws_iam_policy_document" "lambda_assum_role_policy" {
     }
   }
 }
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda-lambdaRole-waf"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
+}
