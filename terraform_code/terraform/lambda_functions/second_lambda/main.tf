18a9b9f2d6228de82a10ce127fd7663379a2ed9a
NormanMueller <93076064+NormanMueller@users.noreply.github.com>
2022-06-10 11:22:22 +0200

    test

---
+++ terraform_code/terraform/lambda_functions/second_lambda/main.tf
@@ -0,0 +1,15 @@
+data "archive_file" "python_lambda_package" {
+  type        = "zip"
+  source_file = "../../lambda_functions/lambda_function_2.py"
+  output_path = "nametest2.zip"
+}
+
+resource "aws_lambda_function" "test_lambda_function" {
+  function_name    = "lambdaTest2"
+  filename         = "nametest2.zip"
+  source_code_hash = data.archive_file.python_lambda_package.output_base64sha256
+  role             = aws_iam_role.lambda_role.arn
+  runtime          = "python3.6"
+  handler          = "lambda_function.lambda_handler"
+  timeout          = 10
+}
