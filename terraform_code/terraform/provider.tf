32aad590a2dd8fcf497dfec0ff71074c9a5d8baa
NormanMueller <93076064+NormanMueller@users.noreply.github.com>
2022-06-10 21:03:31 +0200

    backend terraform s3

--- terraform_code/terraform/provider.tf
+++ terraform_code/terraform/provider.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 0.13"
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "terraform-up-and-running-state-nm-test"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-central-1"
+    bucket = "terraform-up-and-running-state-nm-test"
+    key    = "global/s3/terraform.tfstate"
+    region = "eu-central-1"
     # Replace this with your DynamoDB table name!
     dynamodb_table = "terraform-up-and-running-locks"
     encrypt        = true
